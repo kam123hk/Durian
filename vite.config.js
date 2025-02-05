c94d818cd75469b7ed177968428c7e8b9a3cf2a8
Kam Cheng <kam.cheng03@gmail.com>
2025-02-05 15:51:58 +0000

    Add necessary files before deploying

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Durian/',  // This sets the base URL for GitHub Pages
 })
